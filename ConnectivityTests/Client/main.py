c85188a1df5b60ac1d0f012987c1fa22a89a6b5d
Rahul George <rahuld.george@gmail.com>
2024-02-19 21:55:48 +0100

    fix: got client-server federated learning workinggit add -A!

--- ConnectivityTests/Client/main.py
+++ ConnectivityTests/Client/main.py
@@ -1,4 +1,3 @@
-from Utils.Net import *
 from Utils.Dataset import load_datasets
 from Client.CliUtils import *
 import flwr as fl
@@ -8,5 +7,5 @@ trainloader, valloader, _ = load_datasets()
 client = mk_client(net, trainloader, valloader)
 
 # Connect to the Flower server
-server_address = "apollo.doc.ic.ac.uk:6296"
+server_address = "192.168.1.106:8080" # PLACEHOLDER USED DURING TESTING
 fl.client.start_numpy_client(server_address=server_address, client=client)
