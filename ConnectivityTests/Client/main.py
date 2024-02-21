10ce55c8a2eaf5b9c0987b3f6daf0b57dc2809c3
Rahul George <rahuld.george@gmail.com>
2024-02-21 22:30:24 +0100

    feat: various reimplementations. client-side and server-side logs and parameters, increased number of clients to 2, increased number of epochs, added import corrector

--- ConnectivityTests/Client/main.py
+++ ConnectivityTests/Client/main.py
@@ -1,5 +1,6 @@
 from ConnectivityTests.Utils.Dataset import load_datasets
 from ConnectivityTests.Client.CliUtils import *
+import torch
 import time
 
 net = Net()
@@ -9,11 +10,25 @@ client = mk_client(net, trainloader, valloader)
 # Connect to the Flower server
 server_address = "apollo.doc.ic.ac.uk:6296"
 
-while True:
-    try:
-        fl.client.start_numpy_client(server_address=server_address, client=client)
-        break
-    except Exception as e:
-        print(f"Failed to connect to server: {e}. Retrying in 5 seconds...")
-        time.sleep(5)
-        continue
+fl.client.start_numpy_client(server_address=server_address, client=client)
+
+model_parameters = net.state_dict()
+date_string = time.strftime("%Y-%m-%d_%H-%M-%S")
+path = f"model_params_{date_string}.pth"
+torch.save(model_parameters, path)
+
+
+# logs = client.acc_log
+# with open(f"logs_{date_string}.txt", "w") as f:
+#     f.write(logs)
+#
+
+# Attempted graceful reconnection
+# while True:
+#     try:
+#         fl.client.start_numpy_client(server_address=server_address, client=client)
+#         break
+#     except Exception as e:
+#         print(f"Failed to connect to server: {e}. Retrying in 5 seconds...")
+#         time.sleep(5)
+#         continue
--- ConnectivityTests/Server/main.py
+++ ConnectivityTests/Server/main.py
@@ -4,7 +4,7 @@ import flwr as fl
 from flwr.common import Metrics, FitRes, Parameters, Scalar
 from typing import List, Tuple, Dict, Optional, Union
 from collections import OrderedDict
-import os
+import time
 import torch
 import numpy as np
 from flwr.server.client_proxy import ClientProxy
@@ -21,20 +21,26 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
 
 while True:
     net = Net()
-    c_dir = os.path.dirname(__file__)
-    current_params = "model_params.pth"
-    latest_params = os.path.join(c_dir, current_params)
 
-    if not os.path.exists(latest_params):
-        torch.save(net.state_dict(), latest_params)
-        parameters = fl.common.ndarrays_to_parameters(get_parameters(net))
-    else:
-        print("Loading pre-trained model")
-        state_dict = torch.load(latest_params)
-        net.load_state_dict(state_dict)
-        state_dict_ndarrays = [v.cpu().numpy() for v in net.state_dict().values()]
-        parameters = fl.common.ndarrays_to_parameters(state_dict_ndarrays)
+    parameters = fl.common.ndarrays_to_parameters(get_parameters(net))
 
+    # Deprecated code which was used to save model results.
+
+    # c_dir = os.path.dirname(__file__)
+    # current_params = "model_params.pth"
+    # latest_params = os.path.join(c_dir, current_params)
+
+    # if not os.path.exists(latest_params):
+    #     torch.save(net.state_dict(), latest_params)
+    #     parameters = fl.common.ndarrays_to_parameters(get_parameters(net))
+    # else:
+    #     print("Loading pre-trained model")
+    #     state_dict = torch.load(latest_params)
+    #     net.load_state_dict(state_dict)
+    #     state_dict_ndarrays = [v.cpu().numpy() for v in net.state_dict().values()]
+    #     parameters = fl.common.ndarrays_to_parameters(state_dict_ndarrays)
+
+    date_string = time.strftime("%Y%m%d-%H%M%S")
 
     class SaveModelStrategy(fl.server.strategy.FedAvg):
         def aggregate_fit(
@@ -46,7 +52,7 @@ while True:
             aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
 
             if aggregated_parameters is not None:
-                print(f"Saving aggregated_parameters...")
+                # print(f"Saving aggregated_parameters...")
 
                 aggregated_ndarrays: List[np.ndarray] = fl.common.parameters_to_ndarrays(aggregated_parameters)
 
@@ -54,23 +60,25 @@ while True:
                 state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
                 net.load_state_dict(state_dict, strict=True)
 
-                torch.save(net.state_dict(), f"model_params.pth")
+                if server_round == 100:
+                    torch.save(net.state_dict(), f"final_params_{date_string}.pth")
 
             return aggregated_parameters, aggregated_metrics
-
 
     strategy = SaveModelStrategy(
         fraction_fit=1,
         fraction_evaluate=1,
-        min_fit_clients=1,
-        min_evaluate_clients=1,
-        min_available_clients=1,
+        min_fit_clients=2,
+        min_evaluate_clients=2,
+        min_available_clients=2,
         evaluate_metrics_aggregation_fn=weighted_average,
         initial_parameters=parameters
     )
 
+    fl.common.logger.configure(identifier=f"training_logs_{date_string}", filename=f"log_{date_string}.txt")
+
     fl.server.start_server(
         server_address="apollo.doc.ic.ac.uk:6296",
-        config=fl.server.ServerConfig(num_rounds=3),
+        config=fl.server.ServerConfig(num_rounds=100),
         strategy=strategy
     )
