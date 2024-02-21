381805529d812df0b63b5a2998b713d3c3b6a3e1
Rahul George <rahuld.george@gmail.com>
2024-02-21 22:30:24 +0100

    feat: various reimplementations. client-side and server-side logs and parameters, increased number of clients to 2, increased number of epochs, added import corrector

--- ConnectivityTests/Client/CliUtils.py
+++ ConnectivityTests/Client/CliUtils.py
@@ -2,13 +2,14 @@ import flwr as fl
 from ConnectivityTests.Utils.Net import *
 from ConnectivityTests.Utils.FedParams import *
 
-
 class FlowerClient(fl.client.NumPyClient):
 
     def __init__(self, net, trainloader, valloader):
         self.net = net
         self.trainloader = trainloader
         self.valloader = valloader
+        self.acc_log = ""
+        self.pre_agg_params = None
 
     def get_parameters(self, config):
         return get_parameters(self.net)
@@ -16,12 +17,17 @@ class FlowerClient(fl.client.NumPyClient):
     def fit(self, parameters, config):
         set_parameters(self.net, parameters)
         train(self.net, self.trainloader, epochs=1)
+        self.pre_agg_params = parameters
         return get_parameters(self.net), len(self.trainloader), {}
 
     def evaluate(self, parameters, config):
+        Ploss, Paccuracy = test(self.net, self.valloader)
         set_parameters(self.net, parameters)
-        loss, accuracy = test(self.net, self.valloader)
-        return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
+        Floss, Faccuracy = test(self.net, self.valloader)
+        round = config["current_round"]
+        print(f"ROUND: {round} | LOCAL LOSS & ACCURACY : {float(Ploss)}, {float(Paccuracy) * 100} "
+              f"| GLOBAL LOSS & ACCURACY: {float(Floss)}, {float(Faccuracy) * 100}%\n", flush=True)
+        return float(Floss), len(self.valloader), {"accuracy": float(Faccuracy)}
 
 
 def mk_client(net, trainloader, valloader) -> FlowerClient:
