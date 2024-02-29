3007a6526b45f16dd9ea9075d4156edeebf5fbc5
Rahul George <rahuld.george@gmail.com>
2024-02-29 14:12:39 +0100

    feat: most of implementation for DQN and model

--- TabularAIC1/Training/DQNUtils.py
+++ TabularAIC1/Training/DQNUtils.py
@@ -1,33 +1,38 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim
 
-BASE_OPTIM = torch.optim.Adam(self.parameters(), lr=0.01)
+
 BASE_LAYERS = [128]
 
 
 # Define network used by policy and target
 class offlineDQN(nn.Module):
-    def __init__(self, input_dim, output_dim, layers=BASE_LAYERS, optim=BASE_OPTIM):
-        super(DQN, self).__init__()
+    def __init__(self, input_dim, output_dim, layers=BASE_LAYERS):
+        super().__init__()
 
-        nn_layers = []
-
-        layers.append(nn.Linear(input_dim, layers[0]))
+        nn_layers = [nn.Linear(input_dim, layers[0])]
 
         for i in range(1, len(layers)):
-            in_features = layer_sizes[i-1]
-            out_features = layer_sizes[i]
+            in_features = layers[i-1]
+            out_features = layers[i]
             nn_layers.append(nn.Linear(in_features, out_features))
 
-        layers.append(nn.Linear(layers[-1], output_dim))
+        nn_layers.append(nn.Linear(layers[-1], output_dim))
 
-        self.layers = nn.ModuleList(layers)
-        self.optimizer = optim
+        self.layers = nn.ModuleList(nn_layers)
+
+    def get_parameters(self):
+        return self.layers
+
+    def set_parameters(self, parameters):
+        self.layers = parameters
+
 
     # Forward Propagation
-    def forward(self, state_vector):
+    def forward(self, state_num):
 
-        x = state_vector
+        x = torch.tensor([state_num], dtype=torch.float32)
 
         for num, layer in enumerate(self.layers):
             if num == len(self.layers) - 1:
@@ -38,11 +43,9 @@ class offlineDQN(nn.Module):
         return x
 
     # Backward Propagation
-    def backward(self, target_qs, predicted_qs):
-
-        loss = F.mse_loss(target_qs, predicted_qs)
+    def back_prop(self, target_qs, predicted_qs):
+        loss = F.mse_loss(torch.tensor(target_qs), torch.tensor(predicted_qs))
         loss.backward()
-        self.optimizer.step()
 
     # Compute target Qs
     def compute_target_qs(self, reward, next_state, done, gamma):
@@ -54,7 +57,3 @@ class offlineDQN(nn.Module):
     # Compute predicted Qs
     def compute_predicted_qs(self, state):
         return self.forward(state)
-
-    # Optimize
-    def optimize(self, target_qs, predicted_qs):
-        self.backward(target_qs, predicted_qs)
--- TabularAIC1/Training/Model.py
+++ TabularAIC1/Training/Model.py
@@ -2,6 +2,8 @@ import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import copy
+import torch.optim
 
 from DQNUtils import *
 
@@ -9,22 +11,29 @@ df = pd.read_csv('Preprocessing/df_history.csv')
 EPISODES = df.episode.unique()
 STATES = df.state.unique()
 ACTION_SPACE = df[~df['action'].isnull()]['action'].unique()
-NON_TERMINAL_STATES = df[~df['action'].isnull()]['state'].unique()
-TERMINAL_STATES = df[df['action'].isnull()]['state'].unique()
-
-
-# Instead of initialising a Q dictionary, we will use a DQN model to approximate the Q function, and
-# to predict state-action values. Input: 750 (OHE States), Output: 25 (Actions).
-agent = offlineDQN(750, 25)
 
 N_ITERATIONS = 1000
 BATCH_SIZE = 128
+GAMMA = 0.995
+
+policy = offlineDQN(1, 25)
+target = copy.deepcopy(policy)
 
 # Main Q learning loop.
 for i in range(N_ITERATIONS):
+
+    optimizer = torch.optim.Adam(policy.parameters(), lr=0.01)
 
     batch = df.sample(n=BATCH_SIZE)
 
     predicted, targets = [], []
 
-    #for
+    for index, experience in batch.iterrows():
+        state, action, reward, next_state, done \
+            = (experience['state'], experience['action'], experience['reward'],
+               experience['next_state'], experience['done'])
+
+        predicted.append(policy.compute_predicted_qs(state))
+        targets.append(target.compute_target_qs(reward, next_state, done, GAMMA))
+
+    policy.back_prop(predicted, targets)
