f7b19fe14b36622d5facc1044dfff685e9dd398b
Rahul George <rahuld.george@gmail.com>
2024-02-29 13:42:30 +0100

    feat: added in reprocessed data

--- TabularAIC1/Training/DQNUtils.py
+++ TabularAIC1/Training/DQNUtils.py
@@ -1,94 +1,60 @@
-import math
-import random
-import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
-from collections import namedtuple, deque
 
-Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward', 'done'))
-
-
-class ReplayMemory(object):
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
-
-    def push(self, *args):
-        self.memory.append(Transition(*args))
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
+BASE_OPTIM = torch.optim.Adam(self.parameters(), lr=0.01)
+BASE_LAYERS = [128]
 
 
-class DQN(nn.Module):
-    def __init__(self, inputs, outputs):
+# Define network used by policy and target
+class offlineDQN(nn.Module):
+    def __init__(self, input_dim, output_dim, layers=BASE_LAYERS, optim=BASE_OPTIM):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(inputs, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, outputs)
 
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        nn_layers = []
 
+        layers.append(nn.Linear(input_dim, layers[0]))
 
-class DQNAgent:
-    def __init__(self, state_dim, action_dim, batch_size=128, gamma=0.99, epsilon_start=1.0, epsilon_end=0.01,
-                 epsilon_decay=200, target_update=10, memory_size=10000):
-        self.state_dim = state_dim
-        self.action_dim = action_dim
-        self.batch_size = batch_size
-        self.gamma = gamma
-        self.epsilon_start = epsilon_start
-        self.epsilon_end = epsilon_end
-        self.epsilon_decay = epsilon_decay
-        self.steps_done = 0
-        self.target_update = target_update
+        for i in range(1, len(layers)):
+            in_features = layer_sizes[i-1]
+            out_features = layer_sizes[i]
+            nn_layers.append(nn.Linear(in_features, out_features))
 
-        self.policy_net = DQN(state_dim, action_dim).float()
-        self.target_net = DQN(state_dim, action_dim).float()
-        self.target_net.load_state_dict(self.policy_net.state_dict())
-        self.target_net.eval()
+        layers.append(nn.Linear(layers[-1], output_dim))
 
-        self.optimizer = optim.Adam(self.policy_net.parameters())
-        self.memory = ReplayMemory(memory_size)
+        self.layers = nn.ModuleList(layers)
+        self.optimizer = optim
 
-    def select_action(self, state):
-        sample = random.random()
-        eps_threshold = self.epsilon_end + (self.epsilon_start - self.epsilon_end) * math.exp(
-            -1. * self.steps_done / self.epsilon_decay)
-        self.steps_done += 1
-        if sample > eps_threshold:
-            with torch.no_grad():
-                return self.policy_net(state).max(1)[1].view(1, 1)
-        else:
-            return torch.tensor([[random.randrange(self.action_dim)]], dtype=torch.long)
+    # Forward Propagation
+    def forward(self, state_vector):
 
-    def optimize_model(self):
-        if len(self.memory) < self.batch_size:
-            return
-        transitions = self.memory.sample(self.batch_size)
-        batch = Transition(*zip(*transitions))
+        x = state_vector
 
-        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool)
-        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
-        state_batch = torch.cat(batch.state)
-        action_batch = torch.cat(batch.action)
-        reward_batch = torch.cat(batch.reward)
+        for num, layer in enumerate(self.layers):
+            if num == len(self.layers) - 1:
+                x = layer(x)
+                break
+            x = F.relu(layer(x))
 
-        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+        return x
 
-        next_state_values = torch.zeros(self.batch_size)
-        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
+    # Backward Propagation
+    def backward(self, target_qs, predicted_qs):
 
-        expected_state_action_values = (next_state_values * self.gamma) + reward_batch
-
-        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-
-        self.optimizer.zero_grad()
+        loss = F.mse_loss(target_qs, predicted_qs)
         loss.backward()
         self.optimizer.step()
+
+    # Compute target Qs
+    def compute_target_qs(self, reward, next_state, done, gamma):
+        if done:
+            return reward
+        else:
+            return reward + gamma * torch.max(self.forward(next_state))
+
+    # Compute predicted Qs
+    def compute_predicted_qs(self, state):
+        return self.forward(state)
+
+    # Optimize
+    def optimize(self, target_qs, predicted_qs):
+        self.backward(target_qs, predicted_qs)
--- TabularAIC1/Training/Model.py
+++ TabularAIC1/Training/Model.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from DQNUtils import DQN
+from DQNUtils import *
 
 df = pd.read_csv('Preprocessing/df_history.csv')
 EPISODES = df.episode.unique()
@@ -12,52 +12,19 @@ ACTION_SPACE = df[~df['action'].isnull()]['action'].unique()
 NON_TERMINAL_STATES = df[~df['action'].isnull()]['state'].unique()
 TERMINAL_STATES = df[df['action'].isnull()]['state'].unique()
 
+
 # Instead of initialising a Q dictionary, we will use a DQN model to approximate the Q function, and
 # to predict state-action values. Input: 750 (OHE States), Output: 25 (Actions).
-agent = DQNAgent(750, 25)
+agent = offlineDQN(750, 25)
 
-n_episodes = 1000 # Arbitrary for now.
-reward_per_episode = []
+N_ITERATIONS = 1000
+BATCH_SIZE = 128
 
-for ep in range(len(n_episodes)):
+# Main Q learning loop.
+for i in range(N_ITERATIONS):
 
-    # Choose random episode to train on.
-    ep = np.random.choice(EPISODES)
+    batch = df.sample(n=BATCH_SIZE)
 
-    # Set initial state and reward to 0.
-    s = df[df['episode'] == ep]['state'].iloc[0]
-    episode_reward = 0
+    predicted, targets = [], []
 
-    # Loop through the episode while time-state is not terminal.
-    while s not in TERMINAL_STATES:
-
-        # Create OHE state tensor.
-        state_tensor = torch.zeros(750)
-        state_tensor[s] = 1
-        state = state.unsqueeze(0)
-
-        # Pass state tensor through model to get action tensor.
-        action_tensor = agent.select_action(state)
-        dqn_action = action_tensor.item()  # This is your action
-
-        # I DON'T KNOW HOW I SHOULD BE USING EITHER OF THE ABOVE
-
-        # See what state was actually reached performed by the model, and what the reward was.
-        reward = df[(df['episode'] == ep) & (df['state'] == s)]['reward'].iloc[0]
-        next_state = df[(df['episode'] == ep)
-                        & (df['timestep'] == df[(df['episode'] == ep)
-                                                & (df['state'] == s)]['timestep'].iloc[0] + 1)]['state'].iloc[0]
-
-
-        # Store state-action-reward-next_state-done_flag tuple in memory.
-        agent.memory.push(state, dqn_action, reward, next_state, next_state in TERMINAL_STATES)
-
-        # Sample a batch from memory, compute target Q values for the batch, and compute loss.
-        agent.optimize_model()
-
-        # Increment episode reward and set state to next state.
-        s = next_state
-        episode_reward += reward
-
-    # Append episode reward to list of rewards.
-    reward_per_episode.append(episode_reward)
+    #for
